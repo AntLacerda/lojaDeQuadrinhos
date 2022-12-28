04628dad7e65f72217e40b09908788e8b1f88d79
AntLacerda <lacerdaneto99@hotmail.com>
2022-12-28 14:15:52 -0300

    Adicionando funcionalidade da menssagem de spam

--- script.js
+++ script.js
@@ -52,8 +52,13 @@ const marvel = [
 let sessaoQuadrinhos = document.querySelector("#sessao_quadrinhos");
 let campoPesquisa = document.querySelector("#campo_pesquisa");
 let bolaCarrinho = document.querySelector("#bolinha");
+let barraDeCarregamento = document.querySelector(".carregamento");
+let caixaDeSpam = document.querySelector(".msg_spam");
+let caixaDeSpamTexto = document.querySelector(".msg_do_spam");
+let botaoFecharSpam = document.querySelector("#botao_fechar_spam");
 let pesquisaEmAndamento;
 let arrayLocalStorage = [];
+let movimentacaoBarra;
 
 
 
@@ -61,49 +66,56 @@ function construirCaixa(nome, img, descricao, preco){
     let elementoDiv = document.createElement("div");
     elementoDiv.classList.add("box");
     sessaoQuadrinhos.appendChild(elementoDiv);
-
+    
     let elementoH3 = document.createElement("h3");
     elementoH3.textContent = nome;
     elementoDiv.appendChild(elementoH3);
-
+    
     let elementoImg = document.createElement("img");
     elementoImg.src = img;
     elementoDiv.appendChild(elementoImg);
-
+    
     let elementoH4 = document.createElement("h4");
     elementoH4.textContent = descricao;
     elementoDiv.appendChild(elementoH4);
-
+    
     let elementoDiv2 = document.createElement("div");
     elementoDiv.appendChild(elementoDiv2);
-
+    
     let elementoP = document.createElement("p");
     elementoP.textContent = `R$ ${preco}`;
-
+    
     let elementoImg2 = document.createElement("img");
     elementoImg2.src = "./imgs/botao-de-adicao.png";
-    elementoImg2.addEventListener("click", ()=>{
-
-        
+    elementoImg2.addEventListener("click", ()=>{        
         arrayLocalStorage.push({NOME:nome,IMAGEM:img,DESCRICAO:descricao,PRECO:preco});         //Lembrar de quando for reconstruir os itens no carrinho, se não conseguir construir direito com apenas um array, criar um array pra cada elemento;
-
+        
         localStorage.setItem("quadrinho", JSON.stringify(arrayLocalStorage));
-
-        let teste = JSON.parse(localStorage.quadrinho);
-        console.log(arrayLocalStorage);
-
+        
         let qntDeItens = arrayLocalStorage.length;
         bolaCarrinho.style.display = "block";
         bolaCarrinho.textContent = qntDeItens;
 
-    });
+        caixaDeSpam.style.left = "1150px";
+        
+        let contador = 100;
+        movimentacaoBarra = setInterval(()=>{
+            if(contador==0){
+                clearInterval(movimentacaoBarra);
+                caixaDeSpam.style.left = "1650px";
+            }
+            barraDeCarregamento.style.width = `${contador}%`;
+            contador--;
+        }, 100);
+        let elementoPSpam = document.createElement("p");
+        elementoPSpam.textContent = `O quadrinho: ${nome}, foi adicionado aos pedidos!`;
+        caixaDeSpamTexto.appendChild(elementoPSpam);
 
+        
+    });
+    
     elementoDiv2.appendChild(elementoP);
     elementoDiv2.appendChild(elementoImg2);
-}
-
-function somaCarrinho(){
-    
 }
 
 function pesquisando(){
@@ -142,3 +154,8 @@ function carregarLocalStorage(){
 window.addEventListener("load", carregarLocalStorage);
 window.addEventListener("load", preCarregamento);
 campoPesquisa.addEventListener("keyup", pesquisando);
+
+botaoFecharSpam.addEventListener("click", ()=>{
+    clearInterval(movimentacaoBarra);
+    caixaDeSpam.style.left = "1650px";
+});
